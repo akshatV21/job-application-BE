15ed1720566b7541668f4a933fb7eda8aea37e71
akshatV21 <a.vishwakarma2110@gmail.com>
2022-09-02 10:17:36 +0530

    added job-post schema

---
+++ src/models/jobPost-model.js
@@ -0,0 +1,33 @@
+const { Schema, Types, model } = require("mongoose")
+
+const jobPostSchema = new Schema(
+  {
+    company: {
+      name: { type: String, required: true },
+      email: { type: String },
+      phoneNumber: { type: String },
+      location: {
+        city: { type: String, required: true },
+        state: { type: String, required: true },
+        country: { type: String, required: true },
+      },
+      officialWebsite: { type: String },
+    },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    skills: { type: [String], required: true },
+    jobType: { type: [String], required: true },
+    salary: { type: Number },
+    schedule: { type: String, required: true },
+    remoteWork: { type: Boolean, required: true },
+    education: { type: String },
+    vacancies: { type: Number },
+    applicants: { type: [Types.ObjectId], default: [], ref: "users" },
+    applications: { type: [Types.ObjectId], default: [], ref: "applications" },
+  },
+  { timestamps: true }
+)
+
+const JobPostModel = model("post", jobPostSchema)
+
+module.exports = JobPostModel
